8101002275cf62ca0df027daf99e5e1fb6e9e9e5
ovesh boss <oveshboss.08@gmail.com>
2026-01-09 16:36:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-
 import re
 import os
 from os import environ
@@ -17,73 +16,55 @@ def is_enabled(value, default):
     else:
         return default
       
-# Bot Information
+# --- BOT INFORMATION ---
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/01112d7a3cb9e6fc7daba-6e30b694717486e108.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/01112d7a3cb9e6fc7daba-6e30b694717486e108.jpg')).split() 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "") 
 PORT = environ.get("PORT", "8080")
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
-
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+# --- DATABASE & CLONE INFO ---
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) 
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://bosstgbots_db_user:DiRFdWd2U9kHoP4j@cluster0.g6p3m4j.mongodb.net/?appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
-
-# Database Information
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
-# Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+# --- AUTO DELETE SETTINGS (Fast Cleanup) ---
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) 
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) 
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) 
 
-# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
-
-# Channel Information
+# --- CHANNEL & LOGGING ---
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002908616515"))
 
-# File Caption Information
+# --- CAPTION CUSTOMIZATION (Small Caps) ---
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
-# Enable - True or Disable - False
+# --- PUBLIC ACCESS ---
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-# Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+# --- VERIFICATION SYSTEM ---
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) 
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "") 
+SHORTLINK_API = environ.get("SHORTLINK_API", "") 
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") 
 
-# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+# --- POWERFUL STREAM CONFIG (DISCLOUD FASTEST) ---
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) 
+MULTI_CLIENT = True 
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '10')) 
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
 
-# Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+# Using DisCloud for 10x Speed than Render
+# Slash (/) at the end is added to prevent DNS errors
+URL = environ.get("URL", "https://discloud.app/") 
 
-# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+# --- PERFORMANCE BOOST SETTINGS ---
+WORKERS = int(environ.get("WORKERS", "64")) # Max speed for high traffic
+MAX_CONCURRENT_TRANSMISSIONS = int(environ.get("MAX_CONCURRENT_TRANSMISSIONS", "20"))
 
-# File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
-
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("URL", "https://md-boss-filestoreclone-1.onrender.com/")
-
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-    
